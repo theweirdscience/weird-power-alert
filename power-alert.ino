0dbb59eee8827ccb34cad42fec5418950a33e4cf
Timothy Claassens <tim.claassens@gmail.com>
2018-04-21 20:02:01 +0200

    Working version

--- power-alert.ino
+++ power-alert.ino
@@ -10,6 +10,10 @@
 #define ATTINY85 1 // ATTiny85
 //#define UNO 1      // Arduino UNO
 
+#define MEASURE_INTERVAL 10
+#define HEARTBEAT_INTERVAL 24
+#define GPRS_BAUDRATE 38400
+
 #if defined(UNO)
   #define GPRSPIN 4
   #define RX 2
@@ -19,13 +23,13 @@
   #define GPRSPIN 0
   #define RX 3
   #define TX 4
-  #define VREF 4.97
+  #define VREF 4.943
 #endif
 #define VINPIN A1
 
 /*
  * Max value for adc converter as double
- * The arduino adc converter has a max range of 8-bits resulting in a max value of 1023
+ * The arduino adc converter has a max range of 8-bits resulting in a max value of 1023 
  */
 #define MAXVAL 1023.0
 
@@ -40,7 +44,7 @@ SoftwareSerial SerialAT(RX,TX);
  * Amount by which Vin is divided in the voltage divider 
  * divider = actualVin / actualVout
  */
-#define DIVIDER 6.06
+#define DIVIDER 5.97
 #define QUOTES char(34)
 
 void setup()
@@ -58,10 +62,10 @@ void loop()
     delayWithRelay(3000);
   #endif
   float Vin = measureVoltage();
-  sendData(Vin);                                                  // let server know we are alive at the start of the 24 hour cycle
+  sendData(Vin);                                                                             // let server know we are alive at the start of the 24 hour cycle
   
-  uint32_t timestamp = millis();
-  while (millis() - timestamp < 86400000) {                       // run for 24 hours
+  int cycle_counter = 0;
+  while (cycle_counter < (60/MEASURE_INTERVAL) * HEARTBEAT_INTERVAL) {                       // run for 24 hours
     #if defined(DEVMODE)
       Serial.println("start measuring Vin");
     #endif
@@ -69,7 +73,11 @@ void loop()
     #if defined(DEVMODE)
       Serial.println("wait ten minutes");
     #endif
-    delayWithRelay(600000);                                       // wait 10 minutes before measuring again
+
+    for (int i = 0; i < MEASURE_INTERVAL * 60; i++) {
+      delay(1000);
+    }
+    
     #if defined(DEVMODE)
       Serial.println("measure again");
     #endif
@@ -84,6 +92,7 @@ void loop()
         Serial.println("difference negligible");
       #endif
     }
+    cycle_counter++;
   }
 }
 
@@ -97,25 +106,25 @@ double measureVoltage() {
       delayWithRelay(10);
   }
 
-  return (((float)sum / (float)25 * VREF) / MAXVAL) * DIVIDER;
+  return ((((float)sum / (float)25 * VREF) / MAXVAL) * DIVIDER) + 0.7;
 }
 
 void sendData(float v) {
-  char battery_id[] = "A122233";
+  char battery_id[] = "A122333";
   char vin[5];
   sprintf(vin, "%f", v);
 
-  unsigned int payloadSize = 91;
+  unsigned int payloadSize = 80;
   payloadSize += strlen(battery_id);
   payloadSize += strlen(vin);
   
   GPRSOn();                                                       // Turn on GPRS module
   
-  SerialAT.println("AT+TCPSETUP=0,18.195.219.249,80");            // Connect to server
-  delayWithRelay(3000);
+  SerialAT.println("AT+TCPSETUP=0,85.222.229.221,80");              // Connect to server
+  delay(3000);
   SerialAT.print("AT+TCPSEND=0,");                                // Perform HTTP GET
   SerialAT.println(payloadSize);
-  delayWithRelay(500);
+  delay(2000);
   SerialAT.print("GET /api/battery/car-update/");
   delayWithRelay(10);
   SerialAT.print(v);
@@ -126,7 +135,7 @@ void sendData(float v) {
   delayWithRelay(10);
   SerialAT.print(" HTTP/1.1\n");
   delayWithRelay(10);
-  SerialAT.print("Host: twsba.satyamsaxena.com\n");
+  SerialAT.print("Host: a.phi.guru\n");
   delayWithRelay(10);
   SerialAT.print("Connection: close\n\r\n");
   delayWithRelay(500);
@@ -166,10 +175,12 @@ void GPRSOn() {
     Serial.println("TURNING ON GPRS");
   #endif
   digitalWrite(GPRSPIN, LOW);
-  delayWithRelay(300);
+  delay(300);
   digitalWrite(GPRSPIN, HIGH);
-  SerialAT.begin(115200);
-  delayWithRelay(15000);
+  SerialAT.begin(GPRS_BAUDRATE);
+  for(int i = 0; i < 30; i++){
+    delay(1000);
+  }
   setupGPRS();
 }
 
